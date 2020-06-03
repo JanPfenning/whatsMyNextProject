2428725c92660a5a62a62f6cc38f2c9c04729ed6
I518078 <jan.pfenning@sap.com>
2020-06-03 13:07:55 +0200

    add button to remove a material or tool

--- fe/js/createProject.js
+++ fe/js/createProject.js
@@ -1,7 +1,8 @@
+/*TODO make all those static parent references by a for loop*/
 function changeList(inputElement,type){
     value = inputElement.value;
     row = inputElement.parentNode;
-    if(type==="mat"){
+    if(type===2){
         row = inputElement.parentNode.parentNode;
     }
     container = row.parentNode;
@@ -13,16 +14,17 @@ function changeList(inputElement,type){
     else{
         if(row.nextElementSibling === null){
             newRow = row.cloneNode(true);
-            if(type === "mat"){
+            if(type === 2){
                 newRow.childNodes.forEach(getRow);
-            }else{
+            }else if(type===1){
                 newRow.childNodes.forEach(emptyValues);
+            }else{
+                console.error("We dont know how to handle parameter: "+type)
             }
             container.appendChild(newRow);
         }
     }
 }
-
 function getRow(value){
     if(value.tagName === "SPAN"){
         value.childNodes.forEach(emptyValues)
@@ -32,4 +34,20 @@ function emptyValues(value){
     if(value.tagName==="INPUT"){
         value.value = "";
     }
+}
+/*TODO make those static parent references by a for loop*/
+function removeLine(element,type){
+    if(type===3){
+        e = element.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild;
+    }else if(type===1){
+        e = element.parentNode.previousElementSibling.firstElementChild;
+    }else{
+        console.error("We dont know how to handle parameter: "+type)
+    }
+    e.value = '';
+    e.onchange()
+}
+function getValue(div){
+    div.childNodes.forEach(child.forEach(getValue));
+    return value;
 }
