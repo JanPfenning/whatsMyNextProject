daea0e92b04e1338d88f90638e5fe9b947d94e7f
I518078 <jan.pfenning@sap.com>
2020-06-18 09:13:47 +0200

    cleanup

--- be/src/php/projects.php
+++ be/src/php/projects.php
@@ -13,14 +13,15 @@ if(file_exists($connfile)&&is_readable($connfile)){
 include $path.'/craftXML.php';
 
 /*Simulated Request*/
-$IDvalue = 1;
-// $result = mysqli_query($conn, "select ProjektViewLINK from ProjektlistView where GruppeID = $IDvalue");
-// printXML("Projekte", $result, $conn, $IDvalue);
+//$IDvalue = 1;
+//$result = mysqli_query($conn, "select ProjektViewLINK from ProjektlistView where GruppeID = $IDvalue");
+//printXML("Projekte", $result, $conn, $IDvalue);
 /*if no 'action' default is get*/
 if(!isset($_GET["action"]) || $_GET["action"] == "get"){
     if(isset($_GET["GruppeID"])){
         $IDvalue = $_GET["GruppeID"];
         $result = mysqli_query($conn, "select ProjektViewLINK from ProjektlistView where GruppeID = $IDvalue");
+        //echo('mysqli_query($conn, "select ProjektViewLINK from ProjektlistView where GruppeID = '.$IDvalue.'");');
         $BackgroundURL = mysqli_query($conn, "select BackgroundURL from Bereich as b join Gruppe as g on b.BereichID = g.BereichID where g.GruppeID = $IDvalue"); 
         $BackgroundURLtext = mysqli_fetch_array($BackgroundURL);
         printXML("Projekte", $result, $conn, $IDvalue, "/../../../fe/xslt/overview.xsl", $BackgroundURLtext["BackgroundURL"]);
