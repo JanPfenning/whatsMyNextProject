1af3b65b7055fb7dd6e13fb5a3b1e901bc23935d
Jan Pfenning <jan.pfenning@gmail.com>
2020-07-15 11:21:06 +0200

    Update commentProject.php

--- be/src/php/commentProject.php
+++ be/src/php/commentProject.php
@@ -1,59 +1,22 @@
 <?php
 
-$path = getcwd();
+    $path = getcwd();
 
-include "./linkErrorpage.php";
-$connfile = $path . '/../../../vault/dbConnection.php';
-if(file_exists($connfile)&&is_readable($connfile)){
-    require_once $path . '/../../../vault/dbConnection.php';
-}else{
-    toErrorPage("Failed to load requiered File");
-    die();
-}
-$attributes = $_POST;
-$Nickname = $attributes["nick"];
-$projectID = $attributes["projectID"];
-$comment = $attributes["comment"];
+    include "./linkErrorpage.php";
+    $connfile = $path . '/../../../vault/dbConnection.php';
+    if(file_exists($connfile)&&is_readable($connfile)){
+        require_once $path . '/../../../vault/dbConnection.php';
+    }else{
+        toErrorPage("Failed to load requiered File");
+        die();
+    }
+    $attributes = $_POST;
+    $Nickname = $attributes["nick"];
+    $projectID = $attributes["projectID"];
+    $comment = $attributes["comment"];
 
-$NutzerID = getUserID($Nickname, $conn);
+    $NutzerID = getUserID($Nickname, $conn);
 
-// $query = "INSERT INTO Kommentar (NutzerID,Inhalt) VALUES ('$NutzerID','$comment')";
-// $worked = $conn->query($query);
-
-$query = $conn->prepare("INSERT INTO Kommentar (NutzerID,Inhalt) VALUES (?,?)");
-$query->bind_param("is", $NutzerID, $comment);
-$worked = $query->execute();
-
-if(!$worked){
-    toErrorPage("Error: " . $conn->error."</br> Cannot create comment. Please contact the administrator");
-    die();
-}else{
-    $commentID = $conn->insert_id;
-    // $query = "INSERT INTO Kommentarliste (KommentarlisteID,KommentarViewLINK) VALUES ('$projectID','$commentID');";
-    // $worked = $conn->query($query);
-
-    $query = $conn->prepare("INSERT INTO Kommentarliste (KommentarlisteID,KommentarViewLINK) VALUES (?,?)");
-    $query->bind_param("ii", $projectID, $commentID);
-    echo("INSERT INTO Kommentarliste (KommentarlisteID,KommentarViewLINK) VALUES (?,?) Project ID = $projectID CommentID = $commentID");
-    $worked = $query->execute();
-    $query->close();
-
-<<<<<<< HEAD
-    if(!$worked){
-        $error = $conn->error;
-        // $query = "DELETE FROM Kommentar WHERE KommentarID = $commentID;";
-        // $cleaned = $conn->query($query);
-
-        $query = $conn->prepare("DELETE FROM Kommentar WHERE KommentarID = ?");
-        $query->bind_param("i", $commentID);
-        $cleaned = $query->execute();
-        $query->close();
-
-        if($cleaned){
-            toErrorPage($error);
-        }else{
-            toErrorPage($error."</br> $conn->error </br> comment $commentID not cleaned");
-=======
     // $query = "INSERT INTO Kommentar (NutzerID,Inhalt) VALUES ('$NutzerID','$comment')";
     // $worked = $conn->query($query);
 
@@ -90,12 +53,13 @@ if(!$worked){
             }else{
                 toErrorPage($error."</br> $conn->error </br> comment $commentID not cleaned");
             }
->>>>>>> 20ea223d0ceeb09237d055b8c116782ecbb67599
+        }
+        else{
+            header("Refresh:0");
         }
         $query->close();
     }
-    $query->close();
-}
+
 
     function getUserID($Nickname, $conn){
         $query = "Select NutzerID From Nutzer where Nick = '$Nickname';";
@@ -121,13 +85,7 @@ if(!$worked){
         }else{
             toErrorPage("There are two people with the same nickname, contact the administrator");
             die();
-        }else{
-            return($conn->insert_id);
         }
-    }else{
-        toErrorPage("There are two people with the same nickname, contact the administrator");
-        die();
     }
-}
 
 ?>
