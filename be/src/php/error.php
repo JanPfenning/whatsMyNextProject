80dd7194ee85ed66f24d31477fb17edae0379ff3
I518078 <jan.pfenning@sap.com>
2020-07-19 10:56:13 +0200

    fix create page

--- be/src/php/error.php
+++ be/src/php/error.php
@@ -1,11 +1,9 @@
 <?php
-$xml= '
-<?xml version="1.0" encoding="UTF-8"?>
+$xml= '<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="application/xml" href="../../../fe/xslt/error.xsl"?>
 <!DOCTYPE dataset SYSTEM "http://expensive.click/be/src/dtd/error.dtd">
 <dataset xmlns="http://expensive.click/be/src/dtd/error.dtd">
     <Errormessage>'.$err.'</Errormessage>
-</dataset>
-';
+</dataset>';
 print($xml);
 ?>
