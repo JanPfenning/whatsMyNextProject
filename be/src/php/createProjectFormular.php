d8a55a24b9f17cc5d2f5d5522b8ca4337098c70a
I518078 <jan.pfenning@sap.com>
2020-07-20 10:03:02 +0200

    fix v2

--- be/src/php/createProjectFormular.php
+++ be/src/php/createProjectFormular.php
@@ -1,9 +1,9 @@
 <?php
-$xml= '<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="application/xml" href="/../../../fe/xslt/create.xsl"?>
-<!DOCTYPE dataset SYSTEM "https://expensive.click/be/src/dtd/create.dtd">
-<dataset xmlns="http://expensive.click/be/src/dtd/create.dtd">
-    <GruppeID>'.$_GET["GruppeID"].'</GruppeID>
-</dataset>';
-print($xml);
+header("Content-Type: text/xml");
+print('<?xml version="1.0" encoding="UTF-8"?>');
+print('<?xml-stylesheet type="application/xml" href="/../../../fe/xslt/create.xsl"?>');
+print('<!DOCTYPE dataset SYSTEM "https://expensive.click/be/src/dtd/create.dtd">');
+print('<dataset xmlns="http://expensive.click/be/src/dtd/create.dtd">');
+print('<GruppeID>'.$_GET["GruppeID"].'</GruppeID>');
+print('</dataset>');
 ?>
